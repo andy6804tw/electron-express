d335827dbc0aa8faa98fc1114f35a7daea50ec35
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2019-11-15 17:45:16 +0800

    add loading animate

--- js/index.js
+++ js/index.js
@@ -1,12 +1,32 @@
-var env = Object.create( process.env );
-env.PORT = 3006;
-env.NODE_ENV='development';
+const request = require("request");
 const { ipcRenderer } = require('electron');
-const expressAppUrl = "http://127.0.0.1:3006";
 const spawn = require("child_process").spawn
-const node = spawn("node", ["./api/dist/index.js"], { cwd: process.cwd(), env: env });
-
+let node;
+const env = Object.create( process.env );
 
 ipcRenderer.on('stop-server', (event, data) => {
-node.kill('SIGINT');
+  node.kill('SIGINT');
 });
+
+const addData=()=>{
+  const loading=document.getElementById("loading");
+  const topic=document.getElementById("input_topic").value;
+  const port=document.getElementById("input_port").value;
+  env.PORT = port;
+  env.NODE_ENV='development';
+  node = spawn("node", ["./api/dist/index.js"], { cwd: process.cwd(), env: env });
+  loading.classList.remove("d-none");
+  checkServer(`http://127.0.0.1:${port}`)
+  console.log(port);
+}
+
+const checkServer=(url)=>{
+  let checkServerRunning = setInterval(() => {
+    request(url, (error, response, body) => {
+      if (!error && response.statusCode == 200) {
+        loading.classList.add("d-none");
+        clearInterval(checkServerRunning);
+      }
+    });
+  }, 1000);
+}
